995ceba0ad1bc81c64f9ca4c3a2cdf4eb4fe3cd9
Mike <kjyoung@ualberta.ca>
2015-10-31 15:58:30 -0600

    added opponent edges to input and laid out basic structure of convolutional layer

--- preprocess.py
+++ preprocess.py
@@ -2,11 +2,12 @@ import numpy as np
 """
 raw_games.dat contains a list of moves corresponding to a 13x13 hex game on each line.
 This program converts this into an array of hex positions represented as a 4x17x17
-boolean tensor where the 4 channels represent (in order) white, black, west edge connection
-and east edge connection. The additional width (17x17 instead of 13x13) is to 
-include the edges of the board which are automatically colored for the appropriate
-player and edge connected appropriately, additionally this allows a 5x5 input layer 
-filter to be placed on directly on the edges without going out of range. Every position 
+boolean tensor where the 6 channels represent (in order) white, black, west edge connection,
+east edge connection, opponent north edge connection and opponent south edge
+connection. The additional width (17x17 instead of 13x13) is to include the edges
+of the board which are automatically colored for the appropriate player and edge 
+connected appropriately, additionally this allows a 5x5 input layer filter to be
+placed on directly on the edges without going out of range. Every position 
 is transformed to be white-to-play (by reflection and white-black swap of 
 black-to-play positions). 
 """
@@ -14,6 +15,8 @@ white = 0
 black = 1
 west = 2
 east = 3
+north = 4
+south = 5
 boardsize = 13
 padding = 2
 input_size = boardsize+2*padding
@@ -36,13 +39,15 @@ def flood_fill(game, cell, edge):
 
 
 def new_game():
-	game = np.zeros((4,input_size,input_size), dtype=bool)
+	game = np.zeros((6,input_size,input_size), dtype=bool)
 	game[white, 0:padding, :] = 1
 	game[white, input_size-padding:, :] = 1
 	game[west, 0:padding, :] = 1
 	game[east, input_size-padding:, :] = 1
 	game[black, :, 0:padding] = 1
 	game[black, :, input_size-padding:] = 1
+	game[north, :, 0:padding] = 1
+	game[south, :, input_size-padding:] = 1
 	return game
 
 def cell(move):
@@ -59,20 +64,24 @@ def cell_m(move):
 def play_cell(game, cell, color):
 	x =	cell[0]
 	y = cell[1]
-	west_connection = False
-	east_connection = False
+	edge1_connection = False
+	edge2_connection = False
 	game[color, x, y] = 1
-	#we only track white groups since we are building a network to play as white
 	if(color == white):
-		for n in neighbors((x,y)):
-			if(game[east, n[0], n[1]]):
-				east_connection = True
-			if(game[west, n[0], n[1]]):
-				west_connection = True
-		if(east_connection):
-			flood_fill(game, (x,y), east)
-		if(west_connection):
-			flood_fill(game, (x,y), west)
+		edge1 = east
+		edge2 = west
+	else:
+		edge1 = north
+		edge2 = south
+	for n in neighbors((x,y)):
+		if(game[edge1, n[0], n[1]]):
+			east_connection = True
+		if(game[edge2, n[0], n[1]]):
+			west_connection = True
+	if(edge1_connection):
+		flood_fill(game, (x,y), edge1)
+	if(edge2_connection):
+		flood_fill(game, (x,y), edge2)
 
 def preprocess(filename):
 	infile = open(filename, 'r')
@@ -89,6 +98,7 @@ def preprocess(filename):
 			play_cell(gameW, cell_m(move), black if move_parity else white)
 			move_parity = not move_parity
 			positions.append(np.copy(gameB if move_parity else gameW))
+	return positions
 
 preprocess("raw_games.dat")
 
