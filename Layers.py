56d1c0ab1ce703bc789e7c3830784d4cc6ac3666
Mike <kjyoung@ualberta.ca>
2015-11-10 20:46:41 -0700

    fixed disconnected input error

--- Layers.py
+++ Layers.py
@@ -8,19 +8,39 @@ class HexConvLayer:
         D3_W_bound = np.sqrt(6. / (7*(input_shape[1] + num_D3_filters)))
         D5_W_bound = np.sqrt(6. / (19*(input_shape[1] + num_D5_filters)))
 
-        self.W3_values = theano.shared(np.asarray(rng.uniform(low=-D3_W_bound, high=D3_W_bound, size=(num_D3_filters,input_shape[1],7))), borrow = True)
+        self.W3_values = theano.shared(
+            np.asarray(
+                rng.uniform(
+                    low=-D3_W_bound,
+                    high=D3_W_bound,
+                    size=(num_D3_filters,input_shape[1],7)
+                ),
+                dtype=theano.config.floatX
+            ),
+            borrow = True
+        )
         W3 = T.zeros((num_D3_filters,input_shape[1],3,3))
-        T.set_subtensor(W3[:,:,1:,0], self.W3_values[:,:,:3])
-        T.set_subtensor(W3[:,:,:,1], self.W3_values[:,:,3:6])
-        T.set_subtensor(W3[:,:,:2,2], self.W3_values[:,:,6:])
+        W3 = T.set_subtensor(W3[:,:,1:,0], self.W3_values[:,:,:3])
+        W3 = T.set_subtensor(W3[:,:,:,1], self.W3_values[:,:,3:6])
+        W3 = T.set_subtensor(W3[:,:,:2,2], self.W3_values[:,:,6:])
 
-        self.W5_values = theano.shared(np.asarray(rng.uniform(low=-D3_W_bound, high=D3_W_bound, size=(num_D3_filters,input_shape[1],19))), borrow = True)
+        self.W5_values = theano.shared(
+            np.asarray(
+                rng.uniform(
+                    low=-D3_W_bound,
+                    high=D3_W_bound,
+                    size=(num_D3_filters,input_shape[1],19)
+                ),
+                dtype=theano.config.floatX
+            ),
+            borrow = True
+        )
         W5 = T.zeros((num_D5_filters,input_shape[1],5,5))
-        T.set_subtensor(W5[:,:,2:,0], self.W5_values[:,:,:3])
-        T.set_subtensor(W5[:,:,1:,1], self.W5_values[:,:,3:7])
-        T.set_subtensor(W5[:,:,:,2], self.W5_values[:,:,7:12])
-        T.set_subtensor(W5[:,:,:4,3], self.W5_values[:,:,12:15])
-        T.set_subtensor(W5[:,:,:3,4], self.W5_values[:,:,15:])
+        W5 = T.set_subtensor(W5[:,:,2:,0], self.W5_values[:,:,:3])
+        W5 = T.set_subtensor(W5[:,:,1:,1], self.W5_values[:,:,3:7])
+        W5 = T.set_subtensor(W5[:,:,:,2], self.W5_values[:,:,7:12])
+        W5 = T.set_subtensor(W5[:,:,:4,3], self.W5_values[:,:,12:15])
+        W5 = T.set_subtensor(W5[:,:,:3,4], self.W5_values[:,:,15:])
 
         #TODO: possibly change to use position dependent biases
         b_values = np.zeros((num_D5_filters+num_D3_filters), dtype=theano.config.floatX)
@@ -45,7 +65,7 @@ class HexConvLayer:
         squished_out = T.nnet.relu(full_out + self.b.dimshuffle('x', 0, 'x', 'x'))
 
         padded_out = T.zeros((input_shape[0], num_D3_filters+num_D5_filters, input_shape[2], input_shape[3]))
-        T.set_subtensor(padded_out[:,:,2:-2,2:-2], squished_out)
+        padded_out = T.set_subtensor(padded_out[:,:,2:-2,2:-2], squished_out)
 
         self.output = padded_out
 
