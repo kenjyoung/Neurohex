c73adb165e99bb37a232548c268a832d674b11b9
Mike <kjyoung@ualberta.ca>
2015-12-07 10:24:13 -0700

    added handling for singular value exception

--- scoreDataSet.py
+++ scoreDataSet.py
@@ -7,10 +7,14 @@ print "scoring positions..."
 scores = np.empty((positions.shape[0],boardsize,boardsize))
 num_positions = positions.shape[0]
 output_interval = num_positions/10
-for i in range(num_postions):
+for i in range(num_positions):
 	if(i%output_interval == 0):
 		print "completion: ",i/output_interval
-	scores[i]=score(positions[i], 0)
+	try:
+		scores[i]=score(positions[i], 0)
+	except np.linalg.linalg.LinAlgError:
+		print "singular position at ",str(i),": " state_string(positions[i])
+		score[i]=-1*np.ones((boardsize, boardsize))
 
 print "saving to file..."
 savefile = open("data/scoredPositions.npz", 'w')
