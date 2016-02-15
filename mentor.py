tried to fix another bug in resistance calculation

mentor.py
@@ -21,10 +21,11 @@ parser.add_argument("--save", "-s", type=str, help="Specify a file to save train
 args = parser.parse_args()
 
 print "loading data... "
-datafile = open("data/scoredPositions.npz", 'r')
+datafile = open("data/scoredPositionsFull.npz", 'r')
 data = np.load(datafile)
 positions = data['positions']
 scores = data['scores']
+		
 datafile.close()
 
 

scoreDataSet.py
@@ -12,9 +12,10 @@ for i in range(num_positions):
 		print "completion: ",i/output_interval
 	try:
 		scores[i]=score(positions[i], 0)
+	#if for some reason an uncaught singularity occurs just skip this position
 	except np.linalg.linalg.LinAlgError:
 		print "singular position at ",str(i),": ", state_string(positions[i])
-		scores[i]=-1*np.ones((boardsize, boardsize))
+		i-=1
 
 print "saving to file..."
 savefile = open("data/scoredPositions.npz", 'w')