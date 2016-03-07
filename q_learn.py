c160e0d463b609bbd7786d861a80031f646cb827
Mike <kjyoung@ualberta.ca>
2016-03-06 20:49:16 -0700

    fixed another bug, where unplayable actions were counted in updates

--- q_learn.py
+++ q_learn.py
@@ -16,20 +16,26 @@ def policy(state, evaluator):
 		scores = evaluator(state)
 		#set value of played cells impossibly low so they are never picked
 		scores[played] = -2
+		#print scores.max()
 		return scores.argmax()
 	#choose random open cell
 	return np.random.choice(np.arange(boardsize*boardsize)[np.logical_not(played)])
 
 def Q_update():
 	batch = np.random.choice(np.arange(0,replay_capacity), size=(batch_size))
-	states = state1_memory[batch]
-	scores = evaluate_model_batch(state2_memory[batch])
+	states1 = state1_memory[batch]
+	states2 = state2_memory[batch]
+	scores = evaluate_model_batch(states2)
+	played = np.logical_or(states2[:,white,padding:boardsize+padding,padding:boardsize+padding],\
+		     states2[:,black,padding:boardsize+padding,padding:boardsize+padding]).reshape(scores.shape)
+	#set value of played cells impossibly low so they are never picked
+	scores[played] = -2
 	actions = action_memory[batch]
 	rewards = reward_memory[batch]
 	targets = np.zeros(rewards.size).astype(theano.config.floatX)
 	targets[rewards==1] = 1
 	targets[rewards==0] = -np.amax(scores, axis=1)[rewards==0]
-	cost = train_model(states,targets,actions)
+	cost = train_model(states1,targets,actions)
 	return cost
 
 def action_to_cell(action):
@@ -99,7 +105,7 @@ evaluate_model_batch = theano.function(
 
 cost = T.mean(T.sqr(network.output[T.arange(target_batch.shape[0]),action_batch] - target_batch))
 
-alpha = 0.01
+alpha = 0.001
 rho = 0.9
 epsilon = 1e-6
 updates = rmsprop(cost, network.params, alpha, rho, epsilon)
