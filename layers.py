4b48c6a1311ad76c73a3379bfe87d1c03fc98047
yotomyoto <kjyoung@ualberta.ca>
2016-03-27 23:05:53 -0600

    implementing seperate BN layers and network

--- layers.py
+++ layers.py
@@ -85,6 +85,116 @@ class HexConvLayer:
 
         self.input = input
 
+class HexConvLayerBN:
+    def __init__(self, rng, input, input_shape, num_D5_filters, num_D3_filters, params = None):
+        W3_bound = np.sqrt(6. / (7*(input_shape[1] + num_D3_filters)))
+        W5_bound = np.sqrt(6. / (19*(input_shape[1] + num_D5_filters)))
+
+        if(params):
+        	self.W3_values = params[1]
+        else:
+	        self.W3_values = theano.shared(
+	            np.asarray(
+	                rng.uniform(
+	                    low=-W3_bound,
+	                    high=W3_bound,
+	                    size=(num_D3_filters,input_shape[1],7)
+	                ),
+	                dtype=theano.config.floatX
+	            ),
+	            borrow = True
+	        )
+        #Place weights in hexagonal filter of diameter 3
+        W3 = T.zeros((num_D3_filters,input_shape[1],3,3))
+        W3 = T.set_subtensor(W3[:,:,1:,0], self.W3_values[:,:,:2])
+        W3 = T.set_subtensor(W3[:,:,:,1], self.W3_values[:,:,2:5])
+        W3 = T.set_subtensor(W3[:,:,:2,2], self.W3_values[:,:,5:])
+
+        if(params):
+        	self.W5_values = params[0]
+        else:
+	        self.W5_values = theano.shared(
+	            np.asarray(
+	                rng.uniform(
+	                    low=-W5_bound,
+	                    high=W5_bound,
+	                    size=(num_D5_filters,input_shape[1],19)
+	                ),
+	                dtype=theano.config.floatX
+	            ),
+	            borrow = True
+	        )
+        #Place weights in hexagonal filter of diameter 5
+        W5 = T.zeros((num_D5_filters,input_shape[1],5,5))
+        W5 = T.set_subtensor(W5[:,:,2:,0], self.W5_values[:,:,:3])
+        W5 = T.set_subtensor(W5[:,:,1:,1], self.W5_values[:,:,3:7])
+        W5 = T.set_subtensor(W5[:,:,:,2], self.W5_values[:,:,7:12])
+        W5 = T.set_subtensor(W5[:,:,:4,3], self.W5_values[:,:,12:16])
+        W5 = T.set_subtensor(W5[:,:,:3,4], self.W5_values[:,:,16:])
+
+        if(params):
+        	self.beta = params[2]
+        	self.gamma = params[3]
+        else:
+        	self.gamma = theano.shared(np.zeros((num_D5_filters+num_D3_filters), dtype=theano.config.floatX), borrow=True)
+	        self.beta = theano.shared(np.zeros((num_D5_filters+num_D3_filters), dtype=theano.config.floatX), borrow=True)
+
+        conv_out3 = conv.conv2d(
+            input = input[:,:,1:-1,1:-1],
+            filters = W3,
+            filter_shape = (num_D3_filters,input_shape[1],3,3),
+            image_shape = [input_shape[0], input_shape[1], input_shape[2]-2, input_shape[3]-2]
+        )
+
+        conv_out5 = conv.conv2d(
+            input = input,
+            filters = W5,
+            filter_shape = (num_D5_filters,input_shape[1],5,5),
+            image_shape = input_shape
+        )
+
+        self.full_out = T.concatenate([conv_out5, conv_out3], axis=1)
+
+        self.alpha = alpha
+    	self.running_mean = theano.shared(np.zeros((num_D5_filters+num_D3_filters), dtype=theano.config.floatX), borrow=True)
+    	self.running_std = theano.shared(np.zeros((num_D5_filters+num_D3_filters), dtype=theano.config.floatX), borrow=True)
+    	self.running_mean.default_update = ((1-self.alpha)*self.running_mean+self.alpha*mean)
+    	self.running_std.default_update = ((1-self.alpha)*self.running_std+self.alpha*std)
+
+        self.params = [self.W5_values, self.W3_values, self.b, self.gamma]
+
+        self.mem_size = (T.prod(self.W5_values.shape)+T.prod(self.W3_values.shape)+T.prod(self.b.shape))*4
+
+        self.input = input
+
+        def get_output(self, deterministic = False):
+	    	#test time output
+	    	if(deterministic):
+	        	normed_out= batch_normalization(self.full_out, 
+							self.gamma.dimshuffle('x',0,'x','x'),
+							self.beta.dimshuffle('x',0,'x','x'), 
+							self.running_mean.dimshuffle('x',0,'x','x'), 
+							self.running_std.dimshuffle('x',0,'x','x'))
+	        #training time output
+			else:
+				mean = self.full_out.mean(axis = (0,2,3))
+				std = self.full_out.std(axis = (0,2,3))
+				# include these in computation graph so that their default update will be applied by any function that uses mean and std
+			    # (this idea and much of above implementation borrowed from lasagne)
+			    mean += 0 * self.running_mean
+			    std += 0 * self.running_std
+				normed_out= batch_normalization(self.full_out, 
+							self.gamma.dimshuffle('x',0,'x','x'),
+							self.beta.dimshuffle('x',0,'x','x'), 
+							mean.dimshuffle('x',0,'x','x'), 
+							std.dimshuffle('x',0,'x','x'))
+			squished_out = T.nnet.relu(normed_out)
+
+			padded_out = T.zeros((input_shape[0], num_D3_filters+num_D5_filters, input_shape[2], input_shape[3]))
+			padded_out = T.set_subtensor(padded_out[:,:,2:-2,2:-2], squished_out)
+			return padded_out
+
+
 class FullyConnectedLayer:
     def __init__(self, rng, input, n_in, n_out, params = None):
 		self.input = input
--- network.py
+++ network.py
@@ -145,6 +145,146 @@ class network:
 
 		self.mem_size = layer1.mem_size+layer2.mem_size+layer3.mem_size+layer4.mem_size+layer5.mem_size+layer6.mem_size+ layer7.mem_size + layer8.mem_size + layer9.mem_size + layer10.mem_size
 
+class networkBN:
+	def __init__(self, batch_size = None, rng = None, load_file = None, params = None):
+		if(not rng): rng = np.random.RandomState(None)
+		self.input = T.tensor4('input') #position matrix
+		self.batch_size = batch_size
+		layer0_D3 = 48
+		layer0_D5 = 80
+
+		layer0 = HexConvLayer(
+			rng,
+			self.input, 
+			(batch_size, num_channels, input_size, input_size), 
+			layer0_D5, 
+			layer0_D3,
+			params = params[0:3] if params else None
+		)
+
+		layer1_D3 = 48
+		layer1_D5 = 48
+
+		layer1 = HexConvLayer(
+			rng,
+			layer0.output,
+			(batch_size, layer0_D3+layer0_D5, input_size, input_size),
+			layer1_D5,
+			layer1_D3,
+			params[3:6] if params else None
+		)
+
+		layer2_D3 = 40
+		layer2_D5 = 24
+
+		layer2 = HexConvLayer(
+			rng,
+			layer1.output,
+			(batch_size, layer1_D3+layer1_D5, input_size, input_size),
+			layer2_D5,
+			layer2_D3,
+			params[6:9] if params else None
+		)
+
+		layer3_D3 = 48
+		layer3_D5 = 16
+
+		layer3 = HexConvLayer(
+			rng,
+			layer2.output,
+			(batch_size, layer2_D3+layer2_D5, input_size, input_size),
+			layer3_D5,
+			layer3_D3,
+			params[9:12] if params else None
+		)
+
+		layer4_D3 = 56
+		layer4_D5 = 8
+
+		layer4 = HexConvLayer(
+			rng,
+			layer3.output,
+			(batch_size, layer3_D3+layer3_D5, input_size, input_size),
+			layer4_D5,
+			layer4_D3,
+			params[12:15] if params else None
+		)
+
+		layer5_D3 = 64
+		layer5_D5 = 0
+
+		layer5 = HexConvLayer(
+			rng,
+			layer4.output,
+			(batch_size, layer4_D3+layer4_D5, input_size, input_size),
+			layer5_D5,
+			layer5_D3,
+			params[15:18] if params else None
+		)
+
+		layer6_D3 = 64
+		layer6_D5 = 0
+
+		layer6 = HexConvLayer(
+			rng,
+			layer5.output,
+			(batch_size, layer5_D3+layer5_D5, input_size, input_size),
+			layer6_D5,
+			layer6_D3,
+			params[18:21] if params else None
+		)
+
+		layer7_D3 = 64
+		layer7_D5 = 0
+
+		layer7 = HexConvLayer(
+			rng,
+			layer6.output,
+			(batch_size, layer6_D3+layer6_D5, input_size, input_size),
+			layer7_D5,
+			layer7_D3,
+			params[21:24] if params else None
+		)
+
+		layer8_D3 = 64
+		layer8_D5 = 0
+
+		layer8 = HexConvLayer(
+			rng,
+			layer7.output,
+			(batch_size, layer7_D3+layer7_D5, input_size, input_size),
+			layer8_D5,
+			layer8_D3,
+			params[24:27] if params else None
+		)
+
+		layer9_D3 = 64
+		layer9_D5 = 0
+
+		layer9 = HexConvLayer(
+			rng,
+			layer8.output,
+			(batch_size, layer8_D3+layer8_D5, input_size, input_size),
+			layer9_D5,
+			layer9_D3,
+			params[27:30] if params else None
+		)
+
+		layer10 = SigmoidLayer(
+		 	rng,
+		 	layer9.output.flatten(2),
+		 	(layer9_D3+layer9_D5)*input_size*input_size,
+		 	boardsize*boardsize,
+		 	params[30:32] if params else None
+		)
+
+		self.output = 2*layer10.output-1
+
+		self.params = layer0.params + layer1.params + layer2.params +layer3.params + layer4.params + layer5.params + layer6.params+ layer7.params + layer8.params + layer9.params + layer10.params
+
+		self.mem_size = layer1.mem_size+layer2.mem_size+layer3.mem_size+layer4.mem_size+layer5.mem_size+layer6.mem_size+ layer7.mem_size + layer8.mem_size + layer9.mem_size + layer10.mem_size
+
+
 
 
 
